757d968efd7557f929394978148ae21e4e47937c
hp-programming <controlsystems@saintsrobotics.com>
2019-07-14 16:42:59 -0700

    Deleted SubsystemMap.java
    file not needed

--- src/main/java/frc/robot/SubsystemMap.java
+++
@@ -1,7 +0,0 @@
-import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-public class SubsystemMap {
-    public static DifferentialDrive drivetrain = new DifferentialDrive(RobotMap.leftWheels, RobotMap.rightWheels);
-    public static Subsystem cannonZero = new Cannon
-}
