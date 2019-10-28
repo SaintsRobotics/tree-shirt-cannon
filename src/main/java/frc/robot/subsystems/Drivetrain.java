c088b7c42aa19f414c9701ea08b0d0c00a970d7e
hp-programming <controlsystems@saintsrobotics.com>
2019-10-28 16:05:41 -0700

    Added boost to the drivetrain.

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -3,16 +3,31 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.OI;
 import frc.robot.commands.DrivetrainCommand;
 
 public class Drivetrain extends Subsystem {
     private DifferentialDrive m_differentialDrive;
+    private static final double SPEED_REDUCER = 0.75;
+    private static final double BOOST_COEFECIENT = .9;
 
     public Drivetrain(SpeedController leftMotor, SpeedController rightMotor) {
         m_differentialDrive = new DifferentialDrive(leftMotor, rightMotor);
     }
 
     public void arcadeDrive(double moveSpeed, double rotateSpeed) {
+        if (!OI.xboxController.getRawButton(OI.RIGHT_BUMPER)
+                && !(OI.xboxController.getRawAxis(OI.RIGHT_TRIGGER) > 0.75)) {
+            moveSpeed *= SPEED_REDUCER;
+            rotateSpeed *= SPEED_REDUCER;
+        } else {
+            moveSpeed *= BOOST_COEFECIENT;
+            rotateSpeed *= BOOST_COEFECIENT;
+        }
+
+        SmartDashboard.putNumber("move speed", moveSpeed);
+        SmartDashboard.putNumber("rotate speed", rotateSpeed);
         m_differentialDrive.arcadeDrive(moveSpeed, rotateSpeed);
     }
 
