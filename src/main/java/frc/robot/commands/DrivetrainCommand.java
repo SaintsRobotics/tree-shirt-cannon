c52ba039660c0f9d8d9804de43c09ed1bfec5f7d
LennyAbbas <lennyabbas3@gmail.com>
2021-05-23 15:47:02 -0700

    formatted DrivetrainCommand

--- src/main/java/frc/robot/commands/DrivetrainCommand.java
+++ src/main/java/frc/robot/commands/DrivetrainCommand.java
@@ -1,3 +1,7 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -5,31 +9,35 @@ import frc.robot.OI;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class DrivetrainCommand extends Command {
-    private DrivetrainSubsystem m_drivetrain;
+  private DrivetrainSubsystem m_drivetrain;
 
-    public DrivetrainCommand(DrivetrainSubsystem drivetrain) {
-        m_drivetrain = drivetrain;
-        requires(m_drivetrain);
-    }
+  public DrivetrainCommand(DrivetrainSubsystem drivetrain) {
+    super(drivetrain);
+    m_drivetrain = drivetrain;
+  }
 
-    @Override
-    protected void execute() {
-        this.m_drivetrain.arcadeDrive(-OI.xboxController.getRawAxis(OI.LEFT_STICK_Y),
-                OI.xboxController.getRawAxis(OI.RIGHT_STICK_X));
-    }
+  @Override
+  protected void initialize() {
+  }
 
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
+  @Override
+  protected void execute() {
+    m_drivetrain.arcadeDrive(-OI.xboxController.getRawAxis(OI.LEFT_STICK_Y),
+        OI.xboxController.getRawAxis(OI.RIGHT_STICK_X));
+  }
 
-    @Override
-    protected void end() {
-        this.m_drivetrain.arcadeDrive(0, 0);
-    }
+  @Override
+  protected boolean isFinished() {
+    return false;
+  }
 
-    @Override
-    protected void interrupted() {
-        end();
-    }
+  @Override
+  protected void end() {
+    m_drivetrain.arcadeDrive(0, 0);
+  }
+
+  @Override
+  protected void interrupted() {
+    end();
+  }
 }
