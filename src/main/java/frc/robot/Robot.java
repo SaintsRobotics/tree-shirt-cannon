e6db03f6b5e7b99cb486db26bec47d1f9b3c5164
ProgrammingSR <programming@saintsrobotics.com>
2021-08-18 19:53:39 -0700

    removed shoot on init logic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -90,8 +90,6 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
-
-    m_robotContainer.initializeCannons();
   }
 
   /** This function is called periodically during operator control. */
