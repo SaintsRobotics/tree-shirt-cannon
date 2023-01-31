cff5f587e0ac4b7b25e75ed52a71c4200cb9fd82
ProgrammingSR <programming@saintsrobotics.com>
2023-01-30 19:26:43 -0800

    chore: update wpilib to 2023

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,12 +5,14 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.CannonConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
@@ -21,8 +23,8 @@ import frc.robot.subsystems.DriveSubsystem;
  * This class is where the bulk of the robot should be declared. Since
  * Command-based is a "declarative" paradigm, very little robot logic should
  * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls). Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
+ * scheduler calls). Instead, the structure of the robot (including subsystems
+ * commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   private final XboxController m_controller = new XboxController(OIConstants.kControllerPort);
@@ -35,11 +37,7 @@ public class RobotContainer {
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    configureButtonBindings();
-
-    // Subsystems can not have the same name in the simulator.
-    m_leftCannon.setName("Left Cannon");
-    m_rightCannon.setName("Right Cannon");
+    configureBindings();
 
     // Command that drives with joysticks and boosts with right trigger.
     m_robotDrive.setDefaultCommand(
@@ -55,36 +53,47 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by instantiating a {@link GenericHID} or one of its subclasses
-   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
-   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
    */
-  private void configureButtonBindings() {
+  private void configureBindings() {
     // Fires the left cannon when the A button is pressed (only if the left bumper
     // is held)
-    new JoystickButton(m_controller, Button.kA.value).whenPressed(() -> {
-      if (m_controller.getLeftBumper()) {
-        m_leftCannon.shoot();
-      }
-    });
+    new JoystickButton(m_controller, Button.kA.value).onTrue(
+        new InstantCommand(() -> {
+          if (m_controller.getLeftBumper()) {
+            m_leftCannon.shoot();
+          }
+        }));
 
     // Fires the right cannon when the B button is pressed (only if the left bumper
     // is held)
-    new JoystickButton(m_controller, Button.kB.value).whenPressed(() -> {
-      if (m_controller.getLeftBumper()) {
-        m_rightCannon.shoot();
-      }
-    });
+    new JoystickButton(m_controller, Button.kB.value).onTrue(
+        new InstantCommand(() -> {
+          if (m_controller.getLeftBumper()) {
+            m_rightCannon.shoot();
+          }
+        }));
 
     // Fires both cannons when the X button is pressed (only if the left bumper is
     // held)
-    new JoystickButton(m_controller, Button.kX.value).whenPressed(() -> {
-      if (m_controller.getLeftBumper()) {
-        m_leftCannon.shoot();
-        m_rightCannon.shoot();
-      }
-    });
+    new JoystickButton(m_controller, Button.kX.value).onTrue(
+        new InstantCommand(() -> {
+          if (m_controller.getLeftBumper()) {
+            m_leftCannon.shoot();
+            m_rightCannon.shoot();
+          }
+        }));
   }
 
   /**
